9ef95aede13edcc08fc5a1779aa308ded89d3ebb
Valencua <110689742+Valencua@users.noreply.github.com>
2024-06-09 23:40:43 -0300

    si

--- py/combinados.py
+++ py/combinados.py
@@ -1,16 +1,22 @@
+import tkinter as tk
+from tkinter import messagebox
 from chemlib import Compound, Reaction
-compound = dict["mol":float, "gr": float, "P": float, "V": float, "T": float, "MM": float]
-reactivos: dict[str: compound] = {}
-productos: dict[str: compound] = {}
 
+# Inicialización de diccionarios
+reactivos = {}
+productos = {}
+
+# Constante de gas ideal
 r = 0.082
+
+# Funciones de cálculo (las funciones definidas en el prompt)
 
 def get_by_MM():
     compuestos = [x for x in reactivos.keys() if reactivos[x]["MM"] is not None and reactivos[x]['V'] is not None] + [x for x in productos.keys() if productos[x]["MM"] is not None and productos[x]['V'] is not None]
     if len(compuestos) == 0: return
     
     for compuesto in compuestos:
-        if compuesto in [*reactivos]: reactivos[compuesto]["mol"] = reactivos[compuesto]["V"] * reactivos[compuesto]["MM"]
+        if compuesto in reactivos: reactivos[compuesto]["mol"] = reactivos[compuesto]["V"] * reactivos[compuesto]["MM"]
         else: productos[compuesto]["mol"] = productos[compuesto]["V"] * productos[compuesto]["MM"]
     return
 
@@ -20,7 +26,7 @@ def get_by_MMol():
     if len(compuestos) == 0: return
     
     for compuesto in compuestos:
-        if compuesto in [*reactivos]: reactivos[compuesto]["V"] = reactivos[compuesto]["mol"] / reactivos[compuesto]["MM"]
+        if compuesto in reactivos: reactivos[compuesto]["V"] = reactivos[compuesto]["mol"] / reactivos[compuesto]["MM"]
         else: productos[compuesto]["V"] = productos[compuesto]["mol"] / productos[compuesto]["MM"]
     return
 
@@ -37,176 +43,259 @@ def get_PVT():
 
         if P is None: 
             P = n * T * r / V
-            if compuesto in [*reactivos]: reactivos[compuesto]["P"] = P
-            elif compuesto in [*productos]: productos[compuesto]["P"] = P
+            if compuesto in reactivos: reactivos[compuesto]["P"] = P
+            else: productos[compuesto]["P"] = P
         
         if V is None: 
             V = n * T * r / P
-            if compuesto in [*reactivos]: reactivos[compuesto]["V"] = V
-            elif compuesto in [*productos]: productos[compuesto]["V"] = V
+            if compuesto in reactivos: reactivos[compuesto]["V"] = V
+            else: productos[compuesto]["V"] = V
     
         if T is None: 
             T = n * r / P * V
-            if compuesto in [*reactivos]: reactivos[compuesto]["T"] = T
-            elif compuesto in [*productos]: productos[compuesto]["T"] = T
+            if compuesto in reactivos: reactivos[compuesto]["T"] = T
+            else: productos[compuesto]["T"] = T
 
     return True
 
-def get_masses(reaction: Reaction): #en base a una reacion, una cantidad (de moles o gr) y un compuesto puede sacar el resto de gr y moles de los otros compuestos.
-    
+def get_masses(reaction):
     compuestos = [x for x in reactivos.keys() if reactivos[x]["gr"] is not None or reactivos[x]['mol'] is not None] + [x for x in productos.keys() if productos[x]["mol"] is not None or productos[x]['mol'] is not None]
     if len(compuestos) == 0:
         return False
     
     compuesto = compuestos[0]
 
-    #esta funcion habria que cambiarla para que no acepte mas data que la reaccion. deberia funcionar checkeando si exsiten compuestos con n (mol) o gr que tengan valores definidos
-    
-    listaCompuestos= [*reactivos, *productos]
+    listaCompuestos = [*reactivos, *productos]
 
-    if compuesto in [*reactivos]: 
+    if compuesto in reactivos: 
         if reactivos[compuesto]["mol"] is not None:
             listasa = reaction.get_amounts(listaCompuestos.index(compuesto) + 1, moles = reactivos[compuesto]["mol"])
-
-        else: listasa = reaction.get_amounts(listaCompuestos.index(compuesto) + 1, grams = reactivos[compuesto]["gr"])
-
+        else: 
+            listasa = reaction.get_amounts(listaCompuestos.index(compuesto) + 1, grams = reactivos[compuesto]["gr"])
     else:
         if productos[compuesto]["mol"] is not None:
             listasa = reaction.get_amounts(listaCompuestos.index(compuesto) + 1, moles = productos[compuesto]["mol"])
-
-        else: listasa = reaction.get_amounts(listaCompuestos.index(compuesto) + 1, grams = productos[compuesto]["gr"])
-
+        else: 
+            listasa = reaction.get_amounts(listaCompuestos.index(compuesto) + 1, grams = productos[compuesto]["gr"])
 
     for x in listaCompuestos:
-
-        if x in reactivos.keys(): 
+        if x in reactivos: 
             reactivos[x]["mol"] = listasa[listaCompuestos.index(x)]['moles']
             reactivos[x]["gr"] = listasa[listaCompuestos.index(x)]['grams']
-        elif x in productos.keys(): 
+        elif x in productos: 
             productos[x]["mol"] = listasa[listaCompuestos.index(x)]['moles']
             productos[x]["gr"] = listasa[listaCompuestos.index(x)]['grams']
 
     return get_PVT()
 
-
-
-
-def get_mass_by_PVT(reaction: Reaction): #saca la masa de un compuesto en base a la presion, volumen y temperatura... y r que es una constante
+def get_mass_by_PVT(reaction):
     compuestos = [x for x in reactivos.keys() if reactivos[x]["P"] is not None and reactivos[x]["V"] is not None and reactivos[x]["T"] is not None] + [x for x in productos.keys() if productos[x]["P"] is not None and productos[x]["V"] is not None and productos[x]["T"] is not None]
     if len(compuestos) == 0:
         return False
     
-    compuesto = compuestos[0] if len(compuestos) > 0 else None
+    compuesto = compuestos[0]
 
-    P = reactivos[compuesto]["P"] if compuesto in reactivos.keys() else productos[compuesto]["P"] if compuesto in productos.keys() else None
-    V = reactivos[compuesto]["V"] if compuesto in reactivos.keys() else productos[compuesto]["V"] if compuesto in productos.keys() else None
-    T = reactivos[compuesto]["T"] if compuesto in reactivos.keys() else productos[compuesto]["T"] if compuesto in productos.keys() else None
+    P = reactivos[compuesto]["P"] if compuesto in reactivos else productos[compuesto]["P"]
+    V = reactivos[compuesto]["V"] if compuesto in reactivos else productos[compuesto]["V"]
+    T = reactivos[compuesto]["T"] if compuesto in reactivos else productos[compuesto]["T"]
     
     if P is None or V is None or T is None: 
         return False
     
     mol = P * V / (r * T)
 
-    if compuesto in reactivos.keys(): 
+    if compuesto in reactivos: 
         reactivos[compuesto]["mol"] = mol
         get_masses(reaction)
-    elif compuesto in productos.keys(): 
+    else: 
         productos[compuesto]["mol"] = mol
         get_masses(reaction)
 
-
     return True
 
-def lim_reag(reaction: Reaction):
-    if len([*reactivos]) > 1:
-        a = [x for x in [*reactivos] if reactivos[x]["mol"] is not None]
+def lim_reag(reaction):
+    if len(reactivos) > 1:
+        a = [x for x in reactivos if reactivos[x]["mol"] is not None]
         b = [reactivos[x]["mol"] for x in a]
-        print(x for x in a)
-        print(a, b)
-        if len(a) == len([*reactivos]):
+        if len(a) == len(reactivos):
             l = reaction.limiting_reagent(*b, mode = 'moles')
             return l
-    
         return "No se puede calcular el reactivo limitante"
-        
     return "No hay reactivo limitante en una reaccion de un único reactivo"
 
-
-
-
-
-# Vos tenes que codeart el menu y las funciones de agregar reactivos y productos en abse a los de aca aaaj, es bastanter intuitivo creo. Las parttes de input se remplazan por los inputs de tkinter
-
-
-
-def agregar_reactivos():
-    c   = input("Ingrese el reactivo: ")
-    mol = input("Ingrese la cantidad de moles: ")
-    gr  = input("Ingrese la cantidad de gramos: ")
-    P   = input("Ingrese la presion: ")
-    V   = input("Ingrese el volumen: ")
-    T   = input("Ingrese la temperatura: ")
-    MM   = input("Ingrese la concentración molar: ")
+def agregar_reactivo():
+    c = entry_reactivo.get()
+    mol = entry_mol_reac.get()
+    gr = entry_gr_reac.get()
+    P = entry_P_reac.get()
+    V = entry_V_reac.get()
+    T = entry_T_reac.get()
+    MM = entry_MM_reac.get()
     
-    reactivos[Compound(c).formula] = {
-        "mol": None if mol == '' else float(mol),
-        "gr":  None if gr == '' else float(gr),
-        "P":   None if P == '' else float(P),
-        "V":   None if V == '' else float(V),
-        "T":   None if T == '' else float(T),
-        "MM":  None if MM == '' else float(MM),
-    }
+    if c:
+        reactivos[Compound(c).formula] = {
+            "mol": None if mol == '' else float(mol),
+            "gr": None if gr == '' else float(gr),
+            "P": None if P == '' else float(P),
+            "V": None if V == '' else float(V),
+            "T": None if T == '' else float(T),
+            "MM": None if MM == '' else float(MM),
+        }
+        limpiar_entries_reac()
+        actualizar_reaccion()
 
-    return
-        
-            
-def agregar_productos():
-    c   = input("Ingrese el producto: ")
-    mol = input("Ingrese la cantidad de moles: ")
-    gr  = input("Ingrese la cantidad de gramos: ")
-    P   = input("Ingrese la presion: ")
-    V   = input("Ingrese el volumen: ")
-    T   = input("Ingrese la temperatura: ")
-    MM   = input("Ingrese la concentración molar: ")
-
+def agregar_producto():
+    c = entry_producto.get()
+    mol = entry_mol_prod.get()
+    gr = entry_gr_prod.get()
+    P = entry_P_prod.get()
+    V = entry_V_prod.get()
+    T = entry_T_prod.get()
+    MM = entry_MM_prod.get()
     
-    productos[Compound(c).formula] = {
-        "mol": None if mol == '' else float(mol),
-        "gr":  None if gr == '' else float(gr),
-        "P":   None if P == '' else float(P),
-        "V":   None if V == '' else float(V),
-        "T":   None if T == '' else float(T),
-        "MM":  None if MM == '' else float(MM),
-    }
-
-    return
-
-
+    if c:
+        productos[Compound(c).formula] = {
+            "mol": None if mol == '' else float(mol),
+            "gr": None if gr == '' else float(gr),
+            "P": None if P == '' else float(P),
+            "V": None if V == '' else float(V),
+            "T": None if T == '' else float(T),
+            "MM": None if MM == '' else float(MM),
+        }
+        limpiar_entries_prod()
+        actualizar_reaccion()
 
 def balancear():
     react = " + ".join([*reactivos])
-    prod  = " + ".join([*productos])
-
-    try:  #Inenta generar (chemlib viene con una clase y si no puede generar esa clase es poque el compuesto no existe) la reaccion quimica mediante la formula
+    prod = " + ".join([*productos])
+    
+    try:
         reaccion = Reaction.by_formula(react + "-->" + prod)
         reaccion.balance()
-    except: 
-        print("\nReaccion quimica no valida\n")
+    except Exception as e:
+        messagebox.showerror("Error", "Reacción química no válida\n" + str(e))
         return
     
-    get_mass_by_PVT(reaccion) #aca va una escaera de intento de conseguir la data. se reiteran funciones hasta que se consiga la data necesaria o toda la que se pueda.
-    get_masses(reaccion)
+    get_mass_by_PVT(reaccion)
     get_by_MM()
+    limitante = lim_reag(reaccion)
+    get_masses(reaccion)
     get_by_MMol()
     get_PVT()
+    
+    
 
-    #reactivo limitante
-    limitante = lim_reag(reaccion)
-
-    #si queres rintealos y fijate lo que devuelven
     P = productos
     R = reactivos
 
-    print(limitante, R, P)
+    messagebox.showinfo("Resultado", f"Reactivo limitante: {limitante}\nReactivos: {R}\nProductos: {P}")
 
-    return
+def limpiar_entries_reac():
+    entry_reactivo.delete(0, tk.END)
+    entry_mol_reac.delete(0, tk.END)
+    entry_gr_reac.delete(0, tk.END)
+    entry_P_reac.delete(0, tk.END)
+    entry_V_reac.delete(0, tk.END)
+    entry_T_reac.delete(0, tk.END)
+    entry_MM_reac.delete(0, tk.END)
+
+def limpiar_entries_prod():
+    entry_producto.delete(0, tk.END)
+    entry_mol_prod.delete(0, tk.END)
+    entry_gr_prod.delete(0, tk.END)
+    entry_P_prod.delete(0, tk.END)
+    entry_V_prod.delete(0, tk.END)
+    entry_T_prod.delete(0, tk.END)
+    entry_MM_prod.delete(0, tk.END)
+
+def actualizar_reaccion():
+    react = " + ".join([*reactivos])
+    prod = " + ".join([*productos])
+    reaccion_var.set(f"{react} ---> {prod}")
+
+# Configuración de la interfaz gráfica
+root = tk.Tk()
+root.title("Balanceador de Reacciones Químicas")
+
+reaccion_var = tk.StringVar()
+reaccion_var.set("Reacción: ")
+
+# Sección de Reactivos
+frame_reactivos = tk.LabelFrame(root, text="Reactivos")
+frame_reactivos.grid(row=0, column=0, padx=10, pady=10)
+
+tk.Label(frame_reactivos, text="Reactivo").grid(row=0, column=0)
+entry_reactivo = tk.Entry(frame_reactivos)
+entry_reactivo.grid(row=0, column=1)
+
+tk.Label(frame_reactivos, text="Moles").grid(row=1, column=0)
+entry_mol_reac = tk.Entry(frame_reactivos)
+entry_mol_reac.grid(row=1, column=1)
+
+tk.Label(frame_reactivos, text="Gramos").grid(row=2, column=0)
+entry_gr_reac = tk.Entry(frame_reactivos)
+entry_gr_reac.grid(row=2, column=1)
+
+tk.Label(frame_reactivos, text="Presión").grid(row=3, column=0)
+entry_P_reac = tk.Entry(frame_reactivos)
+entry_P_reac.grid(row=3, column=1)
+
+tk.Label(frame_reactivos, text="Volumen").grid(row=4, column=0)
+entry_V_reac = tk.Entry(frame_reactivos)
+entry_V_reac.grid(row=4, column=1)
+
+tk.Label(frame_reactivos, text="Temperatura").grid(row=5, column=0)
+entry_T_reac = tk.Entry(frame_reactivos)
+entry_T_reac.grid(row=5, column=1)
+
+tk.Label(frame_reactivos, text="Concentración Molar").grid(row=6, column=0)
+entry_MM_reac = tk.Entry(frame_reactivos)
+entry_MM_reac.grid(row=6, column=1)
+
+btn_agregar_reac = tk.Button(frame_reactivos, text="Agregar Reactivo", command=agregar_reactivo)
+btn_agregar_reac.grid(row=7, columnspan=2)
+
+# Sección de Productos
+frame_productos = tk.LabelFrame(root, text="Productos")
+frame_productos.grid(row=0, column=1, padx=10, pady=10)
+
+tk.Label(frame_productos, text="Producto").grid(row=0, column=0)
+entry_producto = tk.Entry(frame_productos)
+entry_producto.grid(row=0, column=1)
+
+tk.Label(frame_productos, text="Moles").grid(row=1, column=0)
+entry_mol_prod = tk.Entry(frame_productos)
+entry_mol_prod.grid(row=1, column=1)
+
+tk.Label(frame_productos, text="Gramos").grid(row=2, column=0)
+entry_gr_prod = tk.Entry(frame_productos)
+entry_gr_prod.grid(row=2, column=1)
+
+tk.Label(frame_productos, text="Presión").grid(row=3, column=0)
+entry_P_prod = tk.Entry(frame_productos)
+entry_P_prod.grid(row=3, column=1)
+
+tk.Label(frame_productos, text="Volumen").grid(row=4, column=0)
+entry_V_prod = tk.Entry(frame_productos)
+entry_V_prod.grid(row=4, column=1)
+
+tk.Label(frame_productos, text="Temperatura").grid(row=5, column=0)
+entry_T_prod = tk.Entry(frame_productos)
+entry_T_prod.grid(row=5, column=1)
+
+tk.Label(frame_productos, text="Concentración Molar").grid(row=6, column=0)
+entry_MM_prod = tk.Entry(frame_productos)
+entry_MM_prod.grid(row=6, column=1)
+
+btn_agregar_prod = tk.Button(frame_productos, text="Agregar Producto", command=agregar_producto)
+btn_agregar_prod.grid(row=7, columnspan=2)
+
+# Botón para balancear
+btn_balancear = tk.Button(root, text="Calcular y Balancear", command=balancear)
+btn_balancear.grid(row=1, columnspan=2, pady=10)
+
+# Mostrar reacción en tiempo real
+lbl_reaccion = tk.Label(root, textvariable=reaccion_var)
+lbl_reaccion.grid(row=2, columnspan=2, pady=10)
+
+root.mainloop()
